20b980b8eb5d0bcac11e34ba94b0cb6d5ad6378b
baranozdemir1 <info@baranozdemir.com>
2024-09-15 21:42:43 +0300

    refactor(project): add headers in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
+    async headers() {
+        return [
+            {
+                source: "/api/v1/:path*",
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Credentials',
+                        value: 'true',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization',
+                    },
+                ],
+            },
+        ];
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+            },
+        ],
+    },
+    experimental: {
+        serverActions: true,
+    },
 };
 
 module.exports = nextConfig;
