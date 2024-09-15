1809fec2ad6e7e5808a17b6708269dc73c4713a7
baranozdemir1 <info@baranozdemir.com>
2024-09-15 22:20:30 +0300

    refactor(project): add headers in next.config.js #6

--- next.config.js
+++ next.config.js
@@ -1,30 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/api/v1/:path*",
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Credentials',
-                        value: 'true',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization',
-                    },
-                ],
-            },
-        ];
-    },
+    crossOrigin: 'anonymous',
     images: {
         remotePatterns: [
             {
